36c7126de03cd7503f34ce8c3904f789647471d8
CeleryControl <controlcelery@gmail.com>
2024-12-24 21:59:11 -0500

    Update updateImageList.js

--- sites/PictureFrame/updateImageList.js
+++ sites/PictureFrame/updateImageList.js
@@ -13,8 +13,14 @@ const files = fs.readdirSync(imgDir).filter(file => {
 const baseURL = 'https://mitchkuebel.github.io/sites/PictureFrame/img/';
 const imageList = files.map(file => `${baseURL}${file}`);
 
+// Create the JSON structure with a count
+const data = {
+    count: imageList.length,
+    images: imageList
+};
+
 // Write the JSON file
 const outputPath = path.join(__dirname, 'imageList.json');
-fs.writeFileSync(outputPath, JSON.stringify(imageList, null, 4));
+fs.writeFileSync(outputPath, JSON.stringify(data, null, 4));
 
-console.log('Image list updated:', imageList);
+console.log('Image list updated with count:', data);
