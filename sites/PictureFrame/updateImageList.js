4b1d418589e176ee89e0a4943ae1c240c43b3b21
CeleryControl <controlcelery@gmail.com>
2024-12-24 20:37:47 -0500

    Update updateImageList.js

--- sites/PictureFrame/updateImageList.js
+++ sites/PictureFrame/updateImageList.js
@@ -1,20 +1,40 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// // Path to the image folder
+// const imgFolder = './img';
+
+// // Get all image files in the folder
+// const images = fs.readdirSync(imgFolder)
+//     .filter(file => file.endsWith('.jpg') || file.endsWith('.png'))
+//     .map(file => `https://mitchkuebel.github.io/sites/PictureFrame/img/${file}`);
+
+// // Create the JSON structure
+// const data = {
+//     count: images.length,
+//     images: images
+// };
+
+// // Write to JSON file
+// fs.writeFileSync('sites/PictureFrame/imageList.json', JSON.stringify(data, null, 4));
+// console.log('imageList.json updated!');
 const fs = require('fs');
 const path = require('path');
 
-// Path to the image folder
-const imgFolder = './img';
+// Adjust this path to match your directory structure
+const imgDir = path.join(__dirname, 'img');
 
-// Get all image files in the folder
-const images = fs.readdirSync(imgFolder)
-    .filter(file => file.endsWith('.jpg') || file.endsWith('.png'))
-    .map(file => `https://mitchkuebel.github.io/sites/PictureFrame/img/${file}`);
+// Read the files in the img directory
+const files = fs.readdirSync(imgDir).filter(file => {
+    return /\.(jpg|jpeg|png|gif)$/i.test(file); // Filter for image files
+});
 
-// Create the JSON structure
-const data = {
-    count: images.length,
-    images: images
-};
+// Build the list of image URLs
+const baseURL = 'https://mitchkuebel.github.io/sites/PictureFrame/img/';
+const imageList = files.map(file => `${baseURL}${file}`);
 
-// Write to JSON file
-fs.writeFileSync('sites/PictureFrame/imageList.json', JSON.stringify(data, null, 4));
-console.log('imageList.json updated!');
+// Write the JSON file
+const outputPath = path.join(__dirname, 'imageList.json');
+fs.writeFileSync(outputPath, JSON.stringify(imageList, null, 4));
+
+console.log('Image list updated:', imageList);
